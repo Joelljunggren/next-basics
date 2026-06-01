e3b8a6221edf90dc7a7cbc1fd7c39d730f1bf12d
Student <joel.ljunggren@hotmail.com>
2026-06-01 10:22:37 +0200

    small additions

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import { ClientButton } from "./_components/client-button"
 export default function AboutPage() {
   return (
     <div className="flex min-h-svh flex-col p-6">
-      <h1 className="text-3xl">About page</h1>
+      <h1 className="text-4xl">About page</h1>
       <article className="mt-4">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor
         temporibus sequi velit obcaecati quo repudiandae modi sapiente atque
---
+++ app/posts/page.tsx
@@ -0,0 +1,8 @@
+export default function PostsPage() {
+  return (
+    <div className="flex min-h-svh flex-col p-6">
+      <h1 className="text-4xl font-bold">Posts</h1>
+      {/* {Render posts here... } */}
+    </div>
+  )
+}
